9ce67484f6d1a03bb4ac061cd7de80a6623b50ac
Sammy <samtang226@gmail.com>
2019-09-16 09:24:14 -0700

    audio testing

--- logic.js
+++ logic.js
@@ -3,3 +3,9 @@ function generateBarCode(){
     var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + nric + '&amp;size=50x50';
                 $('#barcode').attr('src', url);
 }
+
+function generateBarCode2(){
+    var nric = "https://yomyyo.github.io/Video-Auto-Test/audio.html";
+    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + nric + '&amp;size=50x50';
+                $('#barcode').attr('src', url);
+}
